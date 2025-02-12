dcdd35f42545c66bb66cbc75753d241ba665818c
Pius Aaron <piuschbz@gmail.com>
2025-02-12 02:33:36 +0100

    ADD: GET a single book

--- api/routes/books.py
+++ api/routes/books.py
@@ -60,3 +60,12 @@ async def update_book(book_id: int, book: Book) -> Book:
 async def delete_book(book_id: int) -> None:
     db.delete_book(book_id)
     return JSONResponse(status_code=status.HTTP_204_NO_CONTENT, content=None)
+
+
+@router.get("/{book_id}")
+async def get_book(book_id: int) -> Book:
+    book = db.get_book(book_id)
+
+    if not book:
+        return JSONResponse(status_code=status.HTTP_404_NOT_FOUND, content={"detail": "Book not found"})
+    return JSONResponse(status_code=status.HTTP_200_OK, content=book.model_dump())
